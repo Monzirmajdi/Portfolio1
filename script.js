cdd57c6e84cdae9bf40c295143cd7e62210717a2
matrix <bob121953@gmail.com>
2025-09-16 15:45:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -374,9 +374,9 @@ document.addEventListener("DOMContentLoaded", () => {
                     title: "Mr.Crispo - مطعم دجاج مقلي",
                     description: "تصميم شعار لمطعم دجاج مقلي",
                     tools: "Adobe Illustrator, Adobe Photoshop",
-                    previewImage: "images/Branding/mrcrispo/logo01.png",
+                    previewImage: "images/Branding/Mr.crispo/logo09.png",
                     images: [
-                             "images/Branding/mrcrispo/logo01.png"
+                             "images/Branding/Mr.crispo/logo09.png"
                             ]
                 },
                 {
@@ -406,6 +406,24 @@ document.addEventListener("DOMContentLoaded", () => {
                              "images/Branding/Unilab1/logo15.png"  
                             ]   
                 },
+               /* {
+                    title: "Visatchi - وكالة استشارات تأشيرات",
+                    description: "تصميم شعار لوكالة استشارات تأشيرات",
+                    tools: "Adobe Illustrator, Adobe Photoshop",
+                    previewImage: "images/Branding/visatchi/logo01.png",
+                    images: [
+                             "images/Branding/visatchi/logo01.png"
+                            ]
+                },
+                {
+                    title: "Nomadist - معدات خارجية وإكسسوارات سفر",
+                    description: "تصميم شعار لمعدات خارجية وإكسسوارات سفر",
+                    tools: "Adobe Illustrator, Adobe Photoshop",
+                    previewImage: "images/Branding/nomadist/logo01.png",
+                    images: [
+                             "images/Branding/nomadist/logo01.png"
+                            ]
+                },*/
                 {  
                     title: "66 gym & spa- جيم و سبا",  
                     description: "تصميم شعار لمحل جيم و سبا",  
@@ -420,11 +438,15 @@ document.addEventListener("DOMContentLoaded", () => {
                     description: "تصميم شعار لبراند سوداني للاكسسوارات الفضة",
                     tools: "Adobe Illustrator, Adobe Photoshop",
                     previewImage: "images/Branding/Sa/Untitled28_20250624203543_YNhA6ynz1M.webp",
-                    images: ["images/Branding/Sa/Untitled28_20250624203543_YNhA6ynz1M.webp"
+                    images: ["images/Branding/Sa/1_NtqvnVum6y.webp", 
+                             "images/Branding/Sa/2_iabZA5PC8E.webp", 
+                             "images/Branding/Sa/4_GfPbVJRk7O.webp", 
+                             "images/Branding/Sa/5_xheKK6K81L.webp", 
+                             "images/Branding/Sa/6_2PZqglIW4P.webp"]
                 } 
             ]
         },
-          "ui-ux": {
+         "ui-ux": {
             title: "UI/UX Designs",
             items: [
             ]
